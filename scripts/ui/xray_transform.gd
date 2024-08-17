55d1d7483c98b684e3e9d251c323cbee42660bce
Ahnaf30e <auladhossain38@gmail.com>
2024-08-17 21:28:10 +0600

    Changed xray breath value to not match constellation

--- scripts/ui/xray_transform.gd
+++ scripts/ui/xray_transform.gd
@@ -10,6 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	elapsed = wrap(elapsed + delta, 0, 2*PI)
-	var breath = remap(sin(elapsed*2), -1, 1, 1.0, 1.2)
+	var breath = remap(sin(elapsed*1.75), -1, 1, 1.0, 1.2)
 	scale = Vector2.ONE * breath
 	global_position = get_global_mouse_position()
