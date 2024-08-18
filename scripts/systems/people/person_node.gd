b9efd2c3e144368f742fa96688411b38cf94d0d1
wooshdude <parker@cline.cc>
2024-08-17 17:47:04 -0700

    add silly guy

--- scripts/systems/people/person_node.gd
+++ scripts/systems/people/person_node.gd
@@ -1,5 +1,6 @@
 extends Node2D
 class_name PersonNode
+@onready var person_texture: AnimatedSprite2D = $PersonTexture
 
 var xray:Node2D
 
@@ -12,18 +13,13 @@ var person_data:Person :
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	person_texture.play("default")
 	position = Vector2(-64, get_viewport_rect().size.y /2)
 	var new_tween := create_tween()
 	new_tween.tween_property(self, "position", Vector2(get_viewport_rect().size.x /2, position.y), 0.5).set_trans(Tween.TRANS_SINE)
 	new_tween.finished.connect(_on_tween_finished)
 
 	GlobalSignals.next_person.connect(_on_next_person)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("ui_accept"):
-		GlobalSignals.next_person.emit()
 
 
 func _on_next_person():
@@ -34,12 +30,13 @@ func _on_next_person():
 
 func _on_tween_finished():
 	await get_tree().physics_frame
-	print("person %s" % person_data)
-	print("weight: %s" % person_data.weight)
-	print("wealth: %s" % person_data.wealth)
-	print("constellation: %s" % person_data.constellation)
-	print("trade: %s" % person_data.trade)
-	print("belongings: %s" % person_data.belongings)
+	if person_data:
+		print("person %s" % person_data)
+		print("weight: %s" % person_data.weight)
+		print("wealth: %s" % person_data.wealth)
+		print("constellation: %s" % person_data.constellation)
+		print("trade: %s" % person_data.trade)
+		print("belongings: %s" % person_data.belongings)
 	pass
 	
 
--- scripts/ui/person_copy.gd
+++ scripts/ui/person_copy.gd
@@ -1,18 +1,19 @@
-extends Sprite2D
+extends AnimatedSprite2D
 
-@onready var person_texture: Sprite2D = $"../PersonTexture"
+@onready var person_texture: AnimatedSprite2D = $"../PersonTexture"
 @onready var person: PersonNode = $".."
 @onready var xray_insides: BackBufferCopy = $XrayInsides
 
 func _ready() -> void:
-	texture = person_texture.texture
-	person_texture.texture_changed.connect(_on_texture_changed)
+	play("default")
+	sprite_frames = person_texture.sprite_frames
+	person_texture.sprite_frames_changed.connect(_on_texture_changed)
 	
 func _process(delta: float) -> void:
 	if person.xray is Node2D:
 		xray_insides.visible = not person.xray.disabled
 
 func _on_texture_changed():
-	texture = person_texture.texture
+	sprite_frames = person_texture.sprite_frames
 	
 	
